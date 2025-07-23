b774dae1ab66da211888f208ab4033e03fb0349f
ravi <vedularavipramod@gmail.com>
2025-07-23 08:41:17 -0700

    changed the cofig to include nitro : cloudflare-pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
+  nitro: {  
+    preset: 'cloudflare-pages',
+  },
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxt/content',
